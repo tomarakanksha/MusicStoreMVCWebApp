c8dfea69004e7bbceea0cdb1068240b281ff1af9
Mahati Madhira <mm12032@nyu.edu>
2024-07-24 09:48:39 -0500

    changes to UI

--- MusicStoreUI/src/app/app.routes.ts
+++ MusicStoreUI/src/app/app.routes.ts
@@ -1,14 +1,26 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { Component } from '@angular/core';
 import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
 import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
 import { AddAlbumComponent } from './add-album/add-album.component';
+import { HomeComponent } from './home-component/home-component.component';
+import { RegisterComponent } from './register-component/register-component.component';
+import { CartComponent } from './cart-component/cart-component.component';
 
 export const routes: Routes = [
     {path: 'Login', component:LoginComponent},
+    {path: '', component: HomeComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'cart', component: CartComponent},
     { path: 'CustomerDashboard', component: CustomerDashboardComponent },
     { path: 'EmployeeDashboard', component: EmployeeDashboardComponent },
     { path: 'Album', component: AddAlbumComponent},
     {path: 'Logout', component:LoginComponent},
 ];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
---
+++ MusicStoreUI/src/app/cart-component/cart-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart-component.component.html',
+  styleUrl: './cart-component.component.css'
+})
+export class CartComponent {
+
+}
