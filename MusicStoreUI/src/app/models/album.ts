0dd754fe410f9bf4e1f98d0920e7fa69d2d100cf
Akanksha Tomar <akanksha.tomar@incedoinc.com>
2024-07-24 01:32:38 -0500

    add album-details,customer-dashboard,signout component

--- MusicStoreUI/src/app/models/album.ts
+++ MusicStoreUI/src/app/models/album.ts
@@ -1,9 +1,11 @@
 
 export class Album{
     constructor(
-    public AlbumName:string,
-    public Genre:string,
-    public Artist:string,
-    public Duration:number,
+        public albumId:number,
+        public albumName:string,
+        public outletID:number,
+        public outletName:string,
+        public discount:number,
+        public priceAfterDiscount:number,
     ){}
 }
