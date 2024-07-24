c118eb8aed0639af92fdec9e09bcad9c51070ea3
Akanksha Tomar <akanksha.tomar@incedoinc.com>
2024-07-24 01:35:46 -0500

    add album and user service and routes

--- MusicStoreUI/src/app/app.routes.ts
+++ MusicStoreUI/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { LoginComponent } from './login/login.component';
 import { Component } from '@angular/core';
 import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
 import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
-import { AddAlbumComponent } from './add-album/add-album.component';
+import { AlbumDetailsComponent } from './album-details/album-details.component';
+import { SignoutComponent } from './signout/signout.component';
 
 export const routes: Routes = [
     {path: 'Login', component:LoginComponent},
     { path: 'CustomerDashboard', component: CustomerDashboardComponent },
+    {path:'AlbumDetails', component:AlbumDetailsComponent},
     { path: 'EmployeeDashboard', component: EmployeeDashboardComponent },
-    { path: 'Album', component: AddAlbumComponent},
-    {path: 'Logout', component:LoginComponent},
+    {path: 'Logout', component:SignoutComponent},
 ];
